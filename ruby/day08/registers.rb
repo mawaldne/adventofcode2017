9fa09b1f6ee1fac290647639ec3bc8bf1d2a9e01
Mike Waldner <mwaldner@surgeforward.com>
2017-12-13 08:52:26 -0700

    Day 8. EVAL. The only time I use eval is for silly advent of code.

---
+++ ruby/day08/registers.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby -w
+
+# To Run
+# ./registers.rb input.txt
+
+INSTRUCTION = /(.*) (inc|dec) ([-+]?\d*) if (.*) (.*) ([-+]?\d*)$/
+variables = Hash.new(0)
+
+ARGF.each_line do |instruction|
+  if instruction =~ INSTRUCTION
+    var, var2 = $1, $4
+    inc_cmd, boolean = $2, $5
+    inc_amt, criteria = $3, $6
+
+    inc = "+=" if inc_cmd == 'inc'
+    inc = "-=" if inc_cmd == 'dec'
+
+    inst = "variables[var] #{inc} #{inc_amt} if variables[var2] #{boolean} #{criteria}"
+    eval(inst)
+    p inst
+  end
+end
+p variables.values.max
+
