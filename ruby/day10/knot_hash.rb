566e77bfd659f13cea795153c36efbb444d49134
Mike Waldner <mwaldner@surgeforward.com>
2017-12-16 17:26:51 -0700

    Clenaup

--- ruby/day10/knot_hash.rb
+++ ruby/day10/knot_hash.rb
@@ -9,17 +9,17 @@ lengths = ARGF.read.strip.split(',').map(&:to_i)
 list = (0..(256 - 1)).to_a
 
 skip = 0
-cursor = 0
+idx = 0
 
 lengths.each do |length|
-  positions = length.times.map { |i| (cursor + i) % list.length }
+  positions = length.times.map { |i| (idx + i) % list.length }
   sublist = positions.map { |i| list[i] }.reverse
   positions.zip(sublist).each do |position, value|
     list[position] = value
   end
-  cursor = (cursor + length + skip) % list.length
+  idx = (idx + length + skip) % list.length
   skip += 1
 end
 
-pp list[0,2].reduce(&:*)
+pp list[0] * list[1]
 
